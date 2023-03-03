ee8d32bf576ae0c7e8b7b87b08ef835286e5bf4d
kovacsnador <kovacs.nador@gmail.com>
2023-03-03 17:39:39 +0100

    intellisense fix

--- libnador/nador/ui/UiScrollList.h
+++ libnador/nador/ui/UiScrollList.h
@@ -7,57 +7,57 @@
 
 namespace nador
 {
-	class UiScrollList : public IUiElement
-	{
-		enum class EState
-		{
-			IDLE,
-			SCROLLING,
-		};
+    class UiScrollList : public IUiElement
+    {
+        enum class EState
+        {
+            IDLE,
+            SCROLLING,
+        };
 
-		struct Spinner
-		{
-			float_t		lastDeltaTime{ 0 };
-			glm::vec2	lastOffset{ 0, 0 };
-			float_t		deceleration{ 0.85f };
+        struct Spinner
+        {
+            float_t   lastDeltaTime { 0 };
+            glm::vec2 lastOffset { 0, 0 };
+            float_t   deceleration { 0.85f };
 
-			glm::ivec2 Calculate(float_t currdDltaTime);
-		};
+            glm::ivec2 Calculate(float_t currdDltaTime);
+        };
 
-	public:
-		UiScrollList(const glm::ivec2& position, const glm::ivec2& size, UiAlignment alignment = UiAlignment(), IUiElement* parent = nullptr);
+    public:
+        UiScrollList(const glm::ivec2& position, const glm::ivec2& size, UiAlignment alignment = UiAlignment(), IUiElement* parent = nullptr);
 
-		void SetBackground(video::EImageName imageName);
-		void SetSliderColor(const glm::vec4& color);
+        void SetBackground(video::EImageName imageName);
+        void SetSliderColor(const glm::vec4& color);
 
-		UiImage*   GetBackground();
-		UiRoundEdgeOverlay& GetSlider();
-		UiAligner& GetAligner();
+        UiImage*            GetBackground();
+        UiRoundEdgeOverlay& GetSlider();
+        UiAligner&          GetAligner();
 
-		void AddElement(IUiElementPtr element);
+        void AddElement(IUiElementPtr element);
 
-		void OnRender(IRenderer* renderer, const quadVertices_t& vertices) override;
-		void OnRenderEnd(IRenderer* renderer) override;
-		void OnTick(IUiLogicState* uiLogicState) override;
+        void OnRender(IRenderer* renderer, const quadVertices_t& vertices) override;
+        void OnRenderEnd(IRenderer* renderer) override;
+        void OnTick(IUiLogicState* uiLogicState) override;
 
-		void OnMousePressed(EMouseButton mouseButton, const glm::vec2& position) override;
-		void OnMouseReleased(EMouseButton mouseButton, const glm::vec2& position) override;
+        void OnMousePressed(EMouseButton mouseButton, const glm::vec2& position) override;
+        void OnMouseReleased(EMouseButton mouseButton, const glm::vec2& position) override;
 
-	private:
-		void _UpdateSlider();
+    private:
+        void _UpdateSlider();
 
-		EState	   _state{ EState::IDLE };
+        EState _state { EState::IDLE };
 
-		glm::ivec2	_lastMousePos{ 0, 0 };
+        glm::ivec2 _lastMousePos { 0, 0 };
 
-		UiImagePtr _background;
-		UiRoundEdgeOverlay  _slider;
-		UiAligner  _aligner;
-		Spinner	   _spinner;
+        UiImagePtr         _background;
+        UiRoundEdgeOverlay _slider;
+        UiAligner          _aligner;
+        Spinner            _spinner;
 
-		std::list<IUiElementPtr> _elements;
-	};
-	CREATE_PTR_TYPES(UiScrollList);
-}
+        std::list<IUiElementPtr> _elements;
+    };
+    CREATE_PTR_TYPES(UiScrollList);
+} // namespace nador
 
 #endif // !__UI_SCROLL_LIST_H__
