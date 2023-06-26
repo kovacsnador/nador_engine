4e67a18c2aab183e95ef313eb864df83c86a4755
kovacsnador <kovacs.nador@gmail.com>
2023-06-26 21:21:55 +0200

    improve file controller testing

--- libnador/unit_testing/fileController_test.cpp
+++ libnador/unit_testing/fileController_test.cpp
@@ -14,23 +14,39 @@ struct FileControllerTest : public ::testing::TestWithParam<PathPair_t>
     void SetUp() override
     {
         fileCtrl = nador::ModuleFactory::CreateFileController("");
-        const auto& [root, _] = GetParam();
-        EXPECT_TRUE(std::filesystem::create_directories(root));
+        const auto& [path, _] = GetParam();
+
+        std::cout << "Create path: " << path << '\n';
+        EXPECT_TRUE(std::filesystem::create_directories(path));
     }
 
     void TearDown() override
     {
-        const auto& [root, _] = GetParam();
-        EXPECT_TRUE(std::filesystem::remove_all(root));
+        const auto& [path, _] = GetParam();
+
+        std::cout << "Delete path: " << path << '\n';
+        std::cout << "Delete parent path: " << path.parent_path() << '\n';
+        std::cout << "Delete root path: " << path.root_directory() << '\n';
+        std::cout << "Root name path: " << path.root_name() << '\n';
+
+        // getting parent folder if exist
+        auto p = path;
+        while(p.has_parent_path())
+        {
+            p = p.parent_path();
+        }
+
+        // cleanup test folder
+        EXPECT_TRUE(std::filesystem::remove_all(p));
     }
 
     nador::IFileControllerUPtr fileCtrl {nullptr};
 };
 
 INSTANTIATE_TEST_CASE_P(FileControllerTest, FileControllerTest, ::testing::Values(
-    PathPair_t{"root" , "unit_test_file.txt"},
-    PathPair_t{"root" , "unit_test_file2.txt"},
-    PathPair_t{"root" , "unit_test_file3.txt"}));
+    PathPair_t{"unit_test" , "unit_test_file.txt"},
+    //PathPair_t{"unit_test/root" , "unit_test_file2.txt"},
+    PathPair_t{"unit_test/root/sub" , "unit_test_file3.txt"}));
 
 TEST_P(FileControllerTest, WriteAndRead_test)
 {
@@ -81,3 +97,26 @@ TEST_P(FileControllerTest, FileControllerTest_test)
     auto deletedFile = fileCtrl->Read(file);
     EXPECT_FALSE(deletedFile);
 }
+
+TEST_P(FileControllerTest, IsExistAndDelete_test)
+{
+    auto [root, file] = GetParam();
+
+    auto subfolder = root / "newFolder";
+
+    EXPECT_TRUE(fileCtrl->MakeDirs(subfolder));
+    EXPECT_TRUE(fileCtrl->IsExist(subfolder));
+
+    std::string_view helloWorld = "Hello World!";
+    EXPECT_TRUE(fileCtrl->Write(subfolder / file, helloWorld.begin(), helloWorld.size()));
+    EXPECT_TRUE(fileCtrl->IsExist(subfolder / file));
+    EXPECT_TRUE(fileCtrl->Delete(subfolder / file));
+    EXPECT_FALSE(fileCtrl->IsExist(subfolder / file));
+
+    EXPECT_TRUE(fileCtrl->Write(subfolder / file, helloWorld.begin(), helloWorld.size()));
+    EXPECT_TRUE(fileCtrl->IsExist(subfolder / file));
+
+    EXPECT_TRUE(fileCtrl->Delete(subfolder));
+    EXPECT_FALSE(fileCtrl->IsExist(subfolder / file));
+    EXPECT_FALSE(fileCtrl->IsExist(subfolder));
+}   
